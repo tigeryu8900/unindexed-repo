5705ffb921670db01a29552f3dc285e0ee39afa4
tigeryu8900 <tigeryu8900@gmail.com>
2026-02-07 01:42:42 -0800

    fix protobuf

--- extensions/spotify/build.gradle.kts
+++ extensions/spotify/build.gradle.kts
@@ -1,13 +1,44 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.protobuf)
+}
+
 dependencies {
     compileOnly(project(":extensions:shared:library"))
     compileOnly(project(":extensions:shared:stub"))
     compileOnly(project(":extensions:spotify:stub"))
     compileOnly(libs.annotation)
     implementation(libs.hiddenapi)
+
+    //noinspection UseTomlInstead
+    implementation("com.github.ynab:J2V8:6.2.1-16kb.2@aar")
+    implementation(libs.protobuf.javalite)
 }
 
 android {
+    compileSdk = 34
+
     defaultConfig {
-        minSdk = 28
+        minSdk = 23
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+protobuf {
+    protoc {
+        artifact = libs.protobuf.protoc.get().toString()
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                create("java") {
+                    option("lite")
+                }
+            }
+        }
     }
 }
